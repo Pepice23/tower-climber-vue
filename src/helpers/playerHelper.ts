0357a0d4afb9b564573bfdd31ae70f82352d61f8
Molnár József <mojo12984@gmail.com>
2023-02-06 13:42:32 +0100

    Create playerHelper.ts

---
+++ src/helpers/playerHelper.ts
@@ -0,0 +1,9 @@
+export const heroes = [
+  { picturePath: "src/assets/heroes/hero1.jpeg", name: "Hero 1" },
+  { picturePath: "src/assets/heroes/hero2.jpeg", name: "Hero 2" },
+  { picturePath: "src/assets/heroes/hero3.jpeg", name: "Hero 3" },
+  { picturePath: "src/assets/heroes/hero4.jpeg", name: "Hero 4" },
+  { picturePath: "src/assets/heroes/hero5.jpeg", name: "Hero 5" },
+  { picturePath: "src/assets/heroes/hero6.jpeg", name: "Hero 6" },
+  { picturePath: "src/assets/heroes/hero7.jpeg", name: "Hero 7" },
+];
