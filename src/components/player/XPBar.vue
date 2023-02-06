c337ecce865f1658c8492119bd0a641c855681a1
Molnár József <mojo12984@gmail.com>
2023-02-06 19:43:19 +0100

    fix typo, rename store to playerStore

--- src/components/player/XPBar.vue
+++ src/components/player/XPBar.vue
@@ -3,19 +3,21 @@
     <div
       class="progress-bar"
       role="progressbar"
-      :style="{ width: (store.currentXP / store.nextLevelXP) * 100 }"
+      :style="{
+        width: (playerStore.currentXP / playerStore.nextLevelXP) * 100 + '%',
+      }"
       aria-valuemin="1"
       aria-valuemax="100"
     ></div>
     <h5 class="justify-content-center d-flex position-absolute w-100">
-      XP: {{ store.currentXP }}/{{ store.nextLevelXP }}
+      XP: {{ playerStore.currentXP }}/{{ playerStore.nextLevelXP }}
     </h5>
   </div>
 </template>
 
 <script setup lang="ts">
 import { usePlayerStore } from "../../stores/playerStore";
-const store = usePlayerStore();
+const playerStore = usePlayerStore();
 </script>
 
 <style scoped></style>
