f20b17bdcf0830361314eaf10caad00962a76565
Molnár József <mojo12984@gmail.com>
2023-02-06 18:54:02 +0100

    Create CharacterCard.vue

---
+++ src/components/player/CharacterCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col">
+    <h3>Level: {{ store.playerLevel }}</h3>
+    <img
+      :src="store.avatar.picturePath"
+      :alt="store.avatar.name"
+      class="picture-size m-2"
+    />
+    <h3>Damage: {{ store.playerDamage }}</h3>
+    <h3>Defense: {{ store.playerDefense }}</h3>
+    <XPBar />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { usePlayerStore } from "../../stores/playerStore";
+
+const store = usePlayerStore();
+</script>
+
+<style scoped>
+.picture-size {
+  width: 100px;
+  height: 100px;
+}
+</style>
