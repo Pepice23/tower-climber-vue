cefda7cb60d98dd1d8cb200925ff36e5a4e68cf0
Molnár József <mojo12984@gmail.com>
2023-02-06 21:21:23 +0100

    Update ItemShop.vue

--- src/components/game/ItemShop.vue
+++ src/components/game/ItemShop.vue
@@ -1,11 +1,37 @@
 <template>
-<h1>Item Shop</h1>
+  <div class="col m-2 card shop-card-size">
+    <h1>Shop</h1>
+    <div class="row">
+      <div class="col">
+        <button
+          class="btn btn-primary button-size"
+          :disabled="
+            playerStore.money < 2000 ||
+            playerStore.playerInventory.length === 30
+          "
+        >
+          Buy Item
+        </button>
+        <h3>Item price: 2000 gold</h3>
+        <h3>Hero's money: {{ playerStore.money }} gold</h3>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+import { usePlayerStore } from "../../stores/playerStore";
+const playerStore = usePlayerStore();
 </script>
 
 <style scoped>
+.shop-card-size {
+  width: 300px;
+  height: 270px;
+}
 
+.button-size {
+  width: 100px;
+  height: 100px;
+}
 </style>
