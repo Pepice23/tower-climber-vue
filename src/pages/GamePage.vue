f301e6e755e9e7c3eac947cbc4938add6d66f9e8
Molnár József <mojo12984@gmail.com>
2023-02-06 18:55:12 +0100

    Create GamePage.vue

---
+++ src/pages/GamePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row m-2">
+    <StageDisplay />
+    <div class="row m-2">
+      <div class="col">
+        <CharacterCard />
+      </div>
+      <div class="col">
+        <h1>{outcome}</h1>
+      </div>
+      <div class="col">
+        <MonsterCard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import StageDisplay from "../components/game/StageDisplay.vue";
+import MonsterCard from "../components/game/MonsterCard.vue";
+import CharacterCard from "../components/player/CharacterCard.vue";
+import {useMonsterStore} from "../stores/monsterStore";
+
+const monsterStore = useMonsterStore();
+monsterStore.setRandomMonsterAvatar();
+</script>
+
+<style scoped></style>
