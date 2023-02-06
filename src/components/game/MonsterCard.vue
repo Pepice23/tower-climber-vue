3ed754399e19af8fdd1f4dd0ebee84229843936c
Molnár József <mojo12984@gmail.com>
2023-02-06 18:53:53 +0100

    Create MonsterCard.vue

---
+++ src/components/game/MonsterCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col">
+    <h3>Level: {{ playerStore.floor }}</h3>
+    <img
+      :src="monsterStore.monsterAvatar"
+      alt="Monster"
+      class="picture-size m-2"
+    />
+    <h3>Damage: {{ monsterStore.monsterDamage }}</h3>
+    <h3>Defense: {{ monsterStore.monsterDefense }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { usePlayerStore } from "../../stores/playerStore";
+import { useMonsterStore } from "../../stores/monsterStore";
+const playerStore = usePlayerStore();
+const monsterStore = useMonsterStore();
+</script>
+
+<style scoped>
+.picture-size {
+  width: 100px;
+  height: 100px;
+}
+</style>
