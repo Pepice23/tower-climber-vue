84709bf0e6eb53b142607f360e0f1376f604f6f2
Molnár József <mojo12984@gmail.com>
2023-02-06 18:55:52 +0100

    added Game page to routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainMenu from "../pages/MainMenu.vue";
 import CharacterCreator from "../pages/CharacterCreator.vue";
+import GamePage from "../pages/GamePage.vue";
 
 const routes = [
   { path: "/", name: "MainMenu", component: MainMenu },
   { path: "/creator", name: "Creator", component: CharacterCreator },
+  { path: "/game", name: "Game", component: GamePage },
 ];
 
 const router = createRouter({
