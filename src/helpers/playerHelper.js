ec3b11f6964a5ed0fe68ea38cb499909f376ccb5
Molnár József <mojo12984@gmail.com>
2023-02-07 10:11:06 +0100

    fixed picture path

--- src/helpers/playerHelper.js
+++ src/helpers/playerHelper.js
@@ -3,11 +3,11 @@ export function getRandomNumber(min, max) {
 }
 
 export const heroes = [
-  { picturePath: "/heroes/hero1.jpeg", name: "Hero 1" },
-  { picturePath: "/heroes/hero2.jpeg", name: "Hero 2" },
-  { picturePath: "/heroes/hero3.jpeg", name: "Hero 3" },
-  { picturePath: "/heroes/hero4.jpeg", name: "Hero 4" },
-  { picturePath: "/heroes/hero5.jpeg", name: "Hero 5" },
-  { picturePath: "/heroes/hero6.jpeg", name: "Hero 6" },
-  { picturePath: "/heroes/hero7.jpeg", name: "Hero 7" },
+  { picturePath: "src/assets/heroes/hero1.jpeg", name: "Hero 1" },
+  { picturePath: "src/assets/heroes/hero2.jpeg", name: "Hero 2" },
+  { picturePath: "src/assets/heroes/hero3.jpeg", name: "Hero 3" },
+  { picturePath: "src/assets/heroes/hero4.jpeg", name: "Hero 4" },
+  { picturePath: "src/assets/heroes/hero5.jpeg", name: "Hero 5" },
+  { picturePath: "src/assets/heroes/hero6.jpeg", name: "Hero 6" },
+  { picturePath: "src/assets/heroes/hero7.jpeg", name: "Hero 7" },
 ];
