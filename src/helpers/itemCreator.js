3fda6dfcfbdaf84cc201ae6b17d1fc2524f3d0e8
Molnár József <mojo12984@gmail.com>
2023-02-07 14:57:31 +0100

    Update itemCreator.js

--- src/helpers/itemCreator.js
+++ src/helpers/itemCreator.js
@@ -149,43 +149,43 @@ function getRandomRarity() {
 function generateRandomStat(quality, level) {
   const base = 1500;
   if (level >= 1 && level <= 10) {
-    let percent = getRandomNumber(1, 10) + quality * 5;
+    let percent = getRandomNumber(1, 10) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 11 && level <= 20) {
-    let percent = getRandomNumber(11, 20) + quality * 3;
+    let percent = getRandomNumber(11, 20) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 21 && level <= 30) {
-    let percent = getRandomNumber(21, 30) + quality * 3;
+    let percent = getRandomNumber(21, 30) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 31 && level <= 40) {
-    let percent = getRandomNumber(31, 40) + quality * 3;
+    let percent = getRandomNumber(31, 40) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 41 && level <= 50) {
-    let percent = getRandomNumber(41, 50) + quality * 3;
+    let percent = getRandomNumber(41, 50) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 51 && level <= 60) {
-    let percent = getRandomNumber(51, 60) + quality * 3;
+    let percent = getRandomNumber(51, 60) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 61 && level <= 70) {
-    let percent = getRandomNumber(61, 70) + quality * 3;
+    let percent = getRandomNumber(61, 70) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 71 && level <= 80) {
-    let percent = getRandomNumber(71, 80) + quality * 3;
+    let percent = getRandomNumber(71, 80) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 81 && level <= 90) {
-    let percent = getRandomNumber(81, 90) + quality * 3;
+    let percent = getRandomNumber(81, 90) + quality;
     return Math.floor((base * percent) / 100);
   }
   if (level >= 91) {
-    let percent = getRandomNumber(91, 100) + quality * 3;
+    let percent = getRandomNumber(91, 100) + quality;
     return Math.floor((base * percent) / 100);
   }
 }
