2e872403fd0c8fbd67d638ccf6b4909b9e20cff0
Molnár József <mojo12984@gmail.com>
2023-02-07 13:45:15 +0100

    added remove from inventory, totalDamage,totalDefense
    and equip item to make code more DRY

--- src/stores/playerStore.js
+++ src/stores/playerStore.js
@@ -22,6 +22,7 @@ export const usePlayerStore = defineStore("player", {
     addItemToInventory() {
       const item = generateRandomItem(this.playerLevel, this.itemStartId);
       this.playerInventory.push(item);
+      this.itemStartId++;
     },
     compareItems() {
       const equipmentStore = useEquipmentStore();
@@ -59,6 +60,45 @@ export const usePlayerStore = defineStore("player", {
         equipmentStore.necklace
       );
       compareItemsDamageAndDefense(this.playerInventory, equipmentStore.weapon);
+    },
+    removeItemFromInventory(id) {
+      this.playerInventory = this.playerInventory.filter((item) => {
+        return item.id !== id;
+      });
+    },
+    totalDamage() {
+      const equipmentStore = useEquipmentStore();
+      this.playerDamage =
+        equipmentStore.headArmor.equipmentDamage +
+        equipmentStore.shoulderArmor.equipmentDamage +
+        equipmentStore.chestArmor.equipmentDamage +
+        equipmentStore.handArmor.equipmentDamage +
+        equipmentStore.legArmor.equipmentDamage +
+        equipmentStore.footArmor.equipmentDamage +
+        equipmentStore.ring.equipmentDamage +
+        equipmentStore.trinket.equipmentDamage +
+        equipmentStore.necklace.equipmentDamage +
+        equipmentStore.weapon.equipmentDamage;
+    },
+    totalDefense() {
+      const equipmentStore = useEquipmentStore();
+      this.playerDefense =
+        equipmentStore.headArmor.equipmentDefense +
+        equipmentStore.shoulderArmor.equipmentDefense +
+        equipmentStore.chestArmor.equipmentDefense +
+        equipmentStore.handArmor.equipmentDefense +
+        equipmentStore.legArmor.equipmentDefense +
+        equipmentStore.footArmor.equipmentDefense +
+        equipmentStore.ring.equipmentDefense +
+        equipmentStore.trinket.equipmentDefense +
+        equipmentStore.necklace.equipmentDefense +
+        equipmentStore.weapon.equipmentDefense;
+    },
+    equipItem(item) {
+      this.removeItemFromInventory(item.id);
+      this.compareItems();
+      this.totalDamage();
+      this.totalDefense();
     },
   },
 });
