8c300ea9f20df21b89a02176dfa2b6182b4b74f6
Molnár József <mojo12984@gmail.com>
2023-02-10 16:58:19 +0100

    Create CombatScreen.vue

---
+++ src/components/game/CombatScreen.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row m-2">
+    <div class="col">
+      <CharacterCard />
+    </div>
+    <div class="col">
+      <h1>{{ playerStore.outcome }}</h1>
+    </div>
+    <div class="col m-2">
+      <MonsterCard />
+      <div v-if="playerStore.monsterCount === 15">
+        <h3>Time remaining:</h3>
+        <BossTimer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CharacterCard from "../player/CharacterCard.vue";
+import MonsterCard from "./MonsterCard.vue";
+import { usePlayerStore } from "../../stores/playerStore";
+import BossTimer from "../player/BossTimer.vue";
+
+const playerStore = usePlayerStore();
+</script>
+
+<style scoped></style>
