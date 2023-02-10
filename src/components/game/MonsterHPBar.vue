df9d4ec84e373753e1cb7e3f79d741a8e3b1895d
Molnár József <mojo12984@gmail.com>
2023-02-10 16:59:00 +0100

    added two new bars
    Progress for monster HP
    Progress for boss timing

---
+++ src/components/game/MonsterHPBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="progress position-relative text-black" style="height: 30px">
+    <div
+      class="progress-bar bg-success"
+      role="progressbar"
+      :style="{
+        width:
+          (monsterStore.monsterCurrentHP / monsterStore.monsterMaxHP) * 100 +
+          '%',
+      }"
+      aria-valuemin="1"
+      aria-valuemax="100"
+    ></div>
+    <h5 class="justify-content-center d-flex position-absolute w-100">
+      HP: {{ monsterStore.monsterCurrentHP }}
+    </h5>
+  </div>
+</template>
+
+<script setup>
+import { useMonsterStore } from "../../stores/monsterStore.js";
+const monsterStore = useMonsterStore();
+</script>
+
+<style scoped></style>
---
+++ src/components/player/BossTimer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="progress position-relative text-black m-2" style="height: 30px">
+    <div
+      class="progress-bar bg-danger"
+      role="progressbar"
+      :style="{
+        width: (playerStore.bossTimer / playerStore.bossTimerMax) * 100 + '%',
+      }"
+      aria-valuemin="1"
+      aria-valuemax="100"
+    ></div>
+    <h5 class="justify-content-center d-flex position-absolute w-100">
+      {{ playerStore.bossTimer }} sec
+    </h5>
+  </div>
+</template>
+
+<script setup>
+import { usePlayerStore } from "../../stores/playerStore";
+const playerStore = usePlayerStore();
+</script>
+
+<style scoped></style>
