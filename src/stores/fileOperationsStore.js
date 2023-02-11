e97990e708cf85de637dc4ae1502b277113bffac
Molnár József <mojo12984@gmail.com>
2023-02-11 23:10:08 +0100

    Update fileOperationsStore.js

--- src/stores/fileOperationsStore.js
+++ src/stores/fileOperationsStore.js
@@ -8,7 +8,6 @@ import {
 } from "@tauri-apps/api/fs";
 import { usePlayerStore } from "./playerStore";
 import { useEquipmentStore } from "./equipmentStore";
-import { useInventoryStore } from "./inventoryStore.js";
 
 export const useFileOperationsStore = defineStore("fileOperations", {
   state: () => ({}),
@@ -23,8 +22,8 @@ export const useFileOperationsStore = defineStore("fileOperations", {
               avatar: playerStore.avatar,
               level: playerStore.playerLevel,
               money: playerStore.money,
-              damage: playerStore.playerDamage,
-              defense: playerStore.playerDefense,
+              damagePerSec: playerStore.playerDamagePerSecond,
+              damagePerClick: playerStore.playerDamagePerClick,
               currentXP: playerStore.currentXP,
               nextLevelXP: playerStore.nextLevelXP,
               floorNumber: playerStore.floor,
@@ -48,8 +47,10 @@ export const useFileOperationsStore = defineStore("fileOperations", {
         playerStore.avatar = playerDataJson.character.avatar;
         playerStore.playerLevel = playerDataJson.character.level;
         playerStore.money = playerDataJson.character.money;
-        playerStore.playerDamage = playerDataJson.character.damage;
-        playerStore.playerDefense = playerDataJson.character.defense;
+        playerStore.playerDamagePerSecond =
+          playerDataJson.character.damagePerSec;
+        playerStore.playerDamagePerClick =
+          playerDataJson.character.damagePerClick;
         playerStore.currentXP = playerDataJson.character.currentXP;
         playerStore.nextLevelXP = playerDataJson.character.nextLevelXP;
         playerStore.floor = playerDataJson.character.floorNumber;
@@ -64,15 +65,6 @@ export const useFileOperationsStore = defineStore("fileOperations", {
           "tower-climber/equipmentSave.json",
           JSON.stringify({
             equipment: {
-              headArmor: equipmentStore.headArmor,
-              shoulderArmor: equipmentStore.shoulderArmor,
-              chestArmor: equipmentStore.chestArmor,
-              handArmor: equipmentStore.handArmor,
-              legArmor: equipmentStore.legArmor,
-              footArmor: equipmentStore.footArmor,
-              ringArmor: equipmentStore.ring,
-              trinketArmor: equipmentStore.trinket,
-              necklaceArmor: equipmentStore.necklace,
               weapon: equipmentStore.weapon,
             },
           }),
@@ -94,51 +86,7 @@ export const useFileOperationsStore = defineStore("fileOperations", {
           }
         );
         const equipmentJson = JSON.parse(equipment);
-        equipmentStore.headArmor = equipmentJson.equipment.headArmor;
-        equipmentStore.shoulderArmor = equipmentJson.equipment.shoulderArmor;
-        equipmentStore.chestArmor = equipmentJson.equipment.chestArmor;
-        equipmentStore.handArmor = equipmentJson.equipment.handArmor;
-        equipmentStore.legArmor = equipmentJson.equipment.legArmor;
-        equipmentStore.footArmor = equipmentJson.equipment.footArmor;
-        equipmentStore.ring = equipmentJson.equipment.ringArmor;
-        equipmentStore.trinket = equipmentJson.equipment.trinketArmor;
-        equipmentStore.necklace = equipmentJson.equipment.necklaceArmor;
         equipmentStore.weapon = equipmentJson.equipment.weapon;
-      } catch (e) {
-        console.error(e);
-      }
-    },
-    async saveInventoryToFile() {
-      const playerStore = usePlayerStore();
-      const inventoryStore = useInventoryStore();
-      try {
-        await writeFile(
-          "tower-climber/inventorySave.json",
-          JSON.stringify({
-            inventory: inventoryStore.playerInventory,
-            itemStartId: playerStore.itemStartId,
-          }),
-          {
-            dir: BaseDirectory.Document,
-          }
-        );
-      } catch (e) {
-        console.error(e);
-      }
-    },
-    async loadInventoryFromFile() {
-      const playerStore = usePlayerStore();
-      const inventoryStore = useInventoryStore();
-      try {
-        const inventory = await readTextFile(
-          "tower-climber/inventorySave.json",
-          {
-            dir: BaseDirectory.Document,
-          }
-        );
-        const inventoryJson = JSON.parse(inventory);
-        inventoryStore.playerInventory = inventoryJson.inventory;
-        playerStore.itemStartId = inventoryJson.itemStartId;
       } catch (e) {
         console.error(e);
       }
