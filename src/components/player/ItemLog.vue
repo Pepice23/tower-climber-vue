ba0cb0a26272bb9b388a545fa548cc35c9f670c0
Molnár József <mojo12984@gmail.com>
2023-02-12 11:55:23 +0100

    Create ItemLog.vue

---
+++ src/components/player/ItemLog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row m-2">
+    <h1>Item Log</h1>
+    <h3 class="m-2">
+      {{ playerStore.itemLog }}
+    </h3>
+  </div>
+</template>
+
+<script setup>
+import { usePlayerStore } from "../../stores/playerStore.js";
+
+const playerStore = usePlayerStore();
+</script>
+
+<style scoped></style>
