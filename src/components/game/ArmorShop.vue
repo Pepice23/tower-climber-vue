07dbe80a611f63d136466f800e9f78f9790ec6a2
Molnár József <mojo12984@gmail.com>
2023-02-13 11:14:57 +0100

    Create ArmorShop.vue

---
+++ src/components/game/ArmorShop.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Armor Shop</h1>
+  <div class="row">
+    <ArmorShopCard
+      v-for="armor in armors"
+      :key="armor.name"
+      :armor="armor"
+      class="m-2"
+    />
+  </div>
+</template>
+
+<script setup>
+import ArmorShopCard from "./ArmorShopCard.vue";
+import { armors } from "../../helpers/armorHelper.js";
+</script>
+
+<style scoped></style>
