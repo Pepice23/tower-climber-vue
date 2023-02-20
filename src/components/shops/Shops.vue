856826f5949dae14512b2f6612d2bb715fc5eb25
Molnár József <mojo12984@gmail.com>
2023-02-20 08:22:31 +0100

    created UpgradeShop

---
+++ src/components/shops/Shops.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <h1 class="m-2">Shops</h1>
+    <div class="col"><ItemShop /></div>
+    <div class="col"><ArmorShop /></div>
+    <div class="col"><UpgradeShop /></div>
+  </div>
+</template>
+
+<script setup>
+import ItemShop from "../game/ItemShop.vue";
+import ArmorShop from "../game/ArmorShop.vue";
+import UpgradeShop from "../game/UpgradeShop.vue";
+</script>
+
+<style scoped></style>
